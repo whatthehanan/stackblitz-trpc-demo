53bfff00947e93d9f0c053835a9a42b6b1bf1f1f
whatthehanan <hannanhamza@hotmail.com>
2023-03-06 11:49:26 +0100

    setup trpc server

--- server/main.ts
+++ server/main.ts
@@ -1,1 +1,24 @@
-console.log('this is a typescript project');
+import { initTRPC } from '@trpc/server';
+import * as trpcExpress from '@trpc/server/adapters/express';
+import express from 'express';
+
+const t = initTRPC.create();
+const router = t.router;
+
+// initialize router
+const appRouter = router({});
+
+const app = express();
+app.use(
+  '/trpc',
+  trpcExpress.createExpressMiddleware({
+    router: appRouter,
+  })
+);
+
+app.listen(4000);
+
+// this export is used in the client
+// for trpc the recommended way is projects where server and client are in the same directory
+// but I'd assume you could also publish this type in a custom package which the client could use, extra steps to set up ofc
+export type AppRouter = typeof appRouter;
